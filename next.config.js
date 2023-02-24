93cd8f228bbe5097a79651166b32cc1f6cf1e5c8
Seiry Yu <seiry@squareup.com>
2023-02-24 19:04:24 +0800

    add device

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     swcMinify: true,
+    compiler: {
+        // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+        styledComponents: true,
+    },
     poweredByHeader: false,
     experimental: {
         // appDir: false,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,35 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+const Main = styled.main`
+    color: red;
+    .device {
+        overflow: hidden;
+    }
+    article {
+        padding: 1rem;
+        background-color: white;
+        height: 100%;
+        width: 100%;
+        border-radius: 3rem;
+    }
+`
 export default function Home() {
-    return <main>Win!</main>
+    return (
+        <Main>
+            <Head>
+                <title>Win!</title>
+            </Head>
+            <div className="device device-iphone-14-pro">
+                <div className="device-frame">
+                    <article>2121</article>
+                </div>
+                <div className="device-stripe" />
+                <div className="device-header" />
+                <div className="device-sensors" />
+                <div className="device-btns" />
+                <div className="device-power" />
+                <div className="device-home" />
+            </div>
+        </Main>
+    )
 }
